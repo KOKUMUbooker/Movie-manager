57148167a054ac69ffbfcdc5d99798cc878b5b78
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-28 18:41:50 +0300

    Use nav items from admin array

--- UI/src/routes/dashboard/+layout.svelte
+++ UI/src/routes/dashboard/+layout.svelte
@@ -3,12 +3,12 @@
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { userDashboardRoutes } from '@/components/dashboard/dashboard-routes';
+	import { adminDashboardRoutes } from '@/components/dashboard/dashboard-routes';
 	let { children } = $props();
 </script>
 
 <Sidebar.Provider>
-	<AppSidebar navItems={userDashboardRoutes} />
+	<AppSidebar navItems={adminDashboardRoutes} />
 	<Sidebar.Inset>
 		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
 			<Sidebar.Trigger class="-ms-1" />
@@ -26,12 +26,6 @@
 			</Breadcrumb.Root>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4">
-			<!-- <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div class="bg-muted/50 aspect-video rounded-xl"></div>
-        <div class="bg-muted/50 aspect-video rounded-xl"></div>
-        <div class="bg-muted/50 aspect-video rounded-xl"></div>
-      </div>
-      <div class="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min"></div> -->
 			{@render children()}
 		</div>
 	</Sidebar.Inset>
